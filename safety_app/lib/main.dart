dca3fc1d0fcb2e1934a448ebffa7893b7bdf2a98
Ek-04 <wkariuki.e@gmail.com>
2024-06-10 06:09:17 +0300

    commit2

---
+++ safety_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:safety_app/services/auth/home_or_getStarted.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      home: HomeOrGetstarted(),
+    );
+  }
+}
